dee63e45cba7e71de8dcb267dbcd2f76418a7ef4
Mayank Tripathi <mayank.trp48@gmail.com>
2017-10-20 20:06:38 +0530

    Create hb.java

---
+++ hb.java
@@ -0,0 +1,21 @@
+
+public class ValueSelector extends RelativeLayout {
+ 
+    View rootView;
+    TextView valueTextView;
+    View minusButton;
+    View plusButton;
+ 
+    public ValueSelector(Context context) {
+        super(context);
+        init(context);
+    }
+ 
+    public ValueSelector(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+ 
+    private void init(Context context) {
+        //do setup work here
+    }
